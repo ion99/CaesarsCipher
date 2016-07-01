b898c06d072cd454ec9d483c0a1bcdd7bb65da47
Ion Gorincioi <gorincioii@yahoo.com>
2016-06-30 17:54:37 -0700

    JavaScript code solution

--- caesarsCipher.js
+++ caesarsCipher.js
@@ -1,1 +1,13 @@
+function rot13(str) {  
+  let newStr = "";
+  for (let i = 0; i<str.length; i++){
+    if (str[i] === " " || str[i] === "." || str[i] === "," || str[i] === "?" || str[i] === "!"){
+      newStr += str[i];
+    } else if (str.charCodeAt(i) <= 77){
+      newStr += String.fromCharCode(str.charCodeAt(i) + 13);
+    } else newStr += String.fromCharCode(str.charCodeAt(i) - 13);
+ }
+  return newStr;
+}
 
+rot13("LBH QVQ VG!");
